3b1802492c194f075bbfac89ac0413a752c19ed2
Nicole Linde <Collchen@outlook.de>
2021-08-16 17:51:13 +0200

    Added number-handling

--- calculator.js
+++ calculator.js
@@ -4,10 +4,32 @@ let previousOperator;
 
 const screen = document.querySelector('.screen');
 
-function buttonClick() { }
+function buttonClick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
+    }
+    else {
+        handleNumber(value);
+    }
+}
+
+function handleSymbol(symbol) {
+
+}
+
+function handleNumber(numberString) {
+    if (buffer === '0') {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+    screen.innerText = buffer;
+}
 
 function init() { }
-
+document.querySelector('.calc-buttons').addEventListener('click', function (event) {
+    buttonClick(event.target.innerText);
+})
 
 
 init();
